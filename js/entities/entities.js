83d64fb668372d8961c87f9b3f930b8843d3ed95
Raymundo Villegas <memovill@live.com>
2015-03-09 12:04:23 -0700

    NEW

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -7,12 +7,24 @@ game.PlayerEntity = me.Entity.extend({
                 spritewidth: "64",
                 spriteheight: "64",
                 getShape: function(){
-                    return(new me.Rect(0,0, 64, 64).toPolygon);
+                    return(new me.Rect(0,0, 64, 64).toPolygon());
                 }
         }]);
+    
+    this.body.setVelocity(5,20);
+    
     },
     
-    update: function(){
-        
+    update: function(delta){
+       if(me.input.isKeyPressed("right")) {
+         //adds to the position of my x by the velocity defiend above in  
+         //setVelocity () and multiplying it by me.timer.tick
+         //me.timer.tick makes the movement look smooth
+           this.body.vel.x += this.body.accel.x * me.timer.tick;
+       }else{
+           this.body.vel.x = 0;
+       }
+       this.body.update(delta);
+       return true;
     }
 });
