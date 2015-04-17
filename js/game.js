8f54a678dada997c763cca45c1cfacfaac968752
Raymundo Villegas <memovill@live.com>
2015-04-17 11:16:26 -0700

    new

--- js/game.js
+++ js/game.js
@@ -6,19 +6,19 @@ var game = {
 	data : {
 		// score
 		score : 0,
-                enemyBaseHealth:10,
-                playerBaseHealth: 10,
+                enemyBaseHealth:1,
+                playerBaseHealth: 1,
                 enemyCreepHealth:10,
                 playerHealth:10,
                 enemyCreepAttack:1,
-                playerAttack:2,
+                playerAttack:1,
            //     orcBaseDamage: 10,
            //   orcBaseHealth: 100,
            //     orcBaseSpeed: 3,
            //     orcBaseDefense: 0,
                 playerAttackTimer:1000,
                 enemyCreepAttackTimer:1000,
-                playerMoveSpeed:5,
+                playerMoveSpeed:10,
                 creepMoveSpeed: 5,
                 gameTimerManager: "",
                 heroDeathManager: "",      
@@ -28,7 +28,8 @@ var game = {
                 exp1: 0,
                 exp2: 0,
                 exp3: 0,
-                exp4: 0
+                exp4: 0,
+                win: ""
 	},
 	
 	
@@ -46,6 +47,8 @@ var game = {
 			me.plugin.register.defer(this, debugPanel, "debug");
 		});
 	}
+        
+        me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0 });
 
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
@@ -68,6 +71,7 @@ var game = {
             me.pool.register("EnemyCreep", game.EnemyCreep, true);
             me.pool.register("GameTimerManager", game.GameTimerManager);
             me.pool.register("HeroDeathManager" , game.HeroDeathManager);
+            me.pool.register("ExperienceManager" , game.ExperienceManager);
             
 	    me.state.set(me.state.MENU, new game.TitleScreen());
             me.state.set(me.state.PLAY, new game.PlayScreen());
