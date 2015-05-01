80733f9410e08505fcf05aa870c4cd3e39997b15
Raymundo Villegas <memovill@live.com>
2015-04-30 23:03:54 -0700

    mhfhgw

---
+++ php/model/config.php
@@ -0,0 +1,18 @@
+<?php
+require_once(__DIR__ . "/Database.php");
+session_start();
+
+session_regenerate_id(true);
+
+$path = '/RaymundoVAwesomenauts/php/';
+
+$host = "localhost";
+$username = "root";
+$password  = "root";
+$database = "awesomenauts_db";
+
+if(!isset($_SESSION["connection"])){
+    $connection = new Database($host, $username, $password, $database);
+     $_SESSION["connection"] = $connection;
+}
+
